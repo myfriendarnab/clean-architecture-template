1fef069e4bc9a901432315e94b7542335cbcadb4
Arnab Bhattacharya <Arnab.Bhattacharya@maersk.com>
2021-11-18 10:41:37 +0530

    updated Program.cs to simplest without any seeding

--- src/CleanArchitecture.Web/Program.cs
+++ src/CleanArchitecture.Web/Program.cs
@@ -10,33 +10,12 @@ namespace CleanArchitecture.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateWebHostBuilder(args).Build();
+        public static void Main(string[] args) => 
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<AppDbContext>();
-//                    context.Database.Migrate();
-                    context.Database.EnsureCreated();
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
-            }
-
-            host.Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
